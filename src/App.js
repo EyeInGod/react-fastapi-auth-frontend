f9bf20e69382a3e1ce7bac2a3471a256a9e9535f
denis henry <denysmartin4952@gmail.com>
2023-10-23 06:26:06 -0700

    build/login. profile page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React  from 'react';
+import { Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element = {<Login/>}/>
+        <Route path="/profile" element = {<Profile/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
