e4f5f34c55c4a2f870c79652f4ed296e2db2f40d
denis henry <denysmartin4952@gmail.com>
2023-10-23 08:00:01 -0700

    update/login, profile page

--- src/Profile.js
+++ src/Profile.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { useNavigate } from "react-router";
 
 const Profile = () => {
-  return (
-    <div>
-      <h1>profile page</h1>
-    </div>
-  )
+    const navigate = useNavigate();
+
+    const signOut = () => {
+        localStorage.removeItem("temitope");
+        navigate("/");
+    };
+
+    return (
+        <>
+            <div style={{ marginTop: 20, minHeight: 700 }}>
+                <h1>Profile page</h1>
+                <p>Hello there, welcome to your profile page</p>
+
+                <button onClick={signOut}>sign out</button>
+            </div>
+        </>
+    )
 }
 
 export default Profile
