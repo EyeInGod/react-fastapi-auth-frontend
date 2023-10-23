4b3c4aed122a2b677b396a15ad1b2925ed7ed961
denis henry <denysmartin4952@gmail.com>
2023-10-23 07:14:50 -0700

    update/login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React  from 'react';
 import { Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Profile from "./Profile";
+import { RequireToken } from "./Auth";
 import './App.css';
 
 function App() {
@@ -9,7 +10,11 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element = {<Login/>}/>
-        <Route path="/profile" element = {<Profile/>}/>
+        <Route path="/profile" element={
+          <RequireToken>
+            <Profile />
+          </RequireToken>
+        }/>
       </Routes>
     </div>
   );
--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from "react-router";
+import { fetchToken } from "./Auth";
 
 const Login = () => {
-  return (
-    <div>
-      <h1>login page</h1>
-    </div>
-  )
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    //check to see if the fields are not empty
+    const login = () => {
+        if ((username === "") & (password === "")) {
+            return;
+        } else {
+            // make api call to our backend. we'll leave this for later
+        }
+    };
+
+    return (
+        <>
+            <div style={{ minHeight: 800, marginTop: 30 }}>
+                <h1>login page</h1>
+                <div style={{ marginTop: 30 }}>
+                {fetchToken() ? (
+                    <p>you are logged in</p>
+                ) : (
+                    <div>
+                        <form>
+                            <label style={{ marginRight: 10 }}>Input Username</label>
+                            <input
+                                type="text"
+                                onChange={(e) => setUsername(e.target.value)}
+                                />
+
+                            <label style={{ marginRight: 10 }}>Input Password</label>
+                            <input
+                                type="text"
+                                onChange={(e) => setPassword(e.target.value)}
+                                />
+
+                            <button onClick={login}>Login</button>
+                        </form>
+                    </div>
+                )}
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Login
